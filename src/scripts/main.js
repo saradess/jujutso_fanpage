9658a6d92aee8551b5f67f035ed13e570332dad4
Sara Oliveira <sara.desider@gmail.com>
2024-10-27 09:53:59 -0300

    ajuste do toggle da section phrases

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.cards__img');
+    const characters = document.querySelectorAll("[data-phrases-name]");
 
+    //tudo isso pras imagens ampliarem e recolherem -ps lembrete pra nunca mais querer esse efeito em projeto nenhum
     images.forEach(image => {
         let isMobile = window.innerWidth <= 768;
     
@@ -29,4 +31,13 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
     });
-})
+
+
+    function abreOuFechaFrases(e) {
+        e.target.parentNode.classList.toggle("phrases__character__item--is-open");
+    }
+
+    characters.forEach(character => {
+        character.addEventListener("click", abreOuFechaFrases);
+    });
+});
