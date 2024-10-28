00c636f75fe71f65c603ae96c0317128b87479dc
Sara Oliveira <sara.desider@gmail.com>
2024-10-28 07:15:14 -0300

    ajuste da header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.cards__img');
     const characters = document.querySelectorAll("[data-phrases-name]");
 
+    const heroSection = document.querySelector('.hero'); // ajuste para o seletor correto
+    const alturaHero = heroSection.clientHeight;
+
+    //para ocultar a header
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual > alturaHero) {
+            ocultaElementosDoHeader ();
+        } else {
+            exibeElementosDoHeader();
+        }
+        }
+    )
+
     //tudo isso pras imagens ampliarem e recolherem -ps lembrete pra nunca mais querer esse efeito em projeto nenhum
     images.forEach(image => {
         let isMobile = window.innerWidth <= 768;
@@ -32,6 +47,15 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    function ocultaElementosDoHeader () {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+    
+    function exibeElementosDoHeader () {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
 
     function abreOuFechaFrases(e) {
         e.target.parentNode.classList.toggle("phrases__character__item--is-open");
